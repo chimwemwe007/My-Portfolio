ddf0e490ee1eb6f3877fb2330c94a19060572bfe
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-06-16 12:03:03 +0200

    Add functionality to hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const ul = document.querySelector('.links');
 const close = document.querySelector('.close');
+const closeItems = document.querySelector('.desktop-nav');
 
 navToggle.addEventListener('click', () => {
   ul.classList.add('d-block');
@@ -8,4 +9,8 @@ navToggle.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   ul.classList.remove('d-block');
+});
+
+closeItems.addEventListener('click', () => {
+    ul.classList.remove('d-block');    
 });
