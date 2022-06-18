7f7cfd76970eccd6c430181c98a8c72a1ba9b407
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-06-19 00:20:34 +0200

    Add function to preserve data

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,35 @@ close.addEventListener('click', () => {
 closeItems.addEventListener('click', () => {
   ul.classList.remove('d-block');
 });
+
+//Preserve data in browser
+const nameInput = document.getElementById('name')
+const mail = document.getElementById('mail')
+const messageInput = document.getElementById('msg')
+const form = document.getElementById('form')
+
+const inputFields = document.forms['form']
+const formElts = inputFields.querySelectorAll('input, textarea');
+
+// Local storage 
+
+const saveToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+const formData = getFromLocalStorage('formData');
+if (formData !== null) {
+  nameInput.value = formData.name;
+  mail.value = formData.email;
+  messageInput.value = formData.message;
+}
+formElts.forEach((fe) => {
+  fe.addEventListener('input', () => {
+    // errorMessage.style.display = 'none';
+    const objectForLocalStorage = {
+      name: nameInput.value,
+      email: mail.value,
+      message: messageInput.value,
+    };
+    saveToLocalStorage('formData', objectForLocalStorage);
+  });
+});
