c798b2a3ca8c15bfae67d14707fe937f4b501869
krisbobo <kiyongbohbo@gmail.com>
2022-06-18 15:02:40 +0100

    Add email validation

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ closeItems.addEventListener('click', () => {
   ul.classList.remove('d-block');
 });
 
-/* form validation*/
+// form validation
 
 const isLowerCase = (str) => /[a-z]/.test(str) && !/[A-Z]/.test(str);
 const form = document.querySelector('#contact-form');
@@ -23,8 +23,19 @@ const form = document.querySelector('#contact-form');
 const email = document.querySelector('#mail');
 const errorHolder = document.querySelector('.emailError');
 
-email.addEventListener('click',()=> {
-  errorHolder.classList.remove('d-flex')
-})
+email.addEventListener('click', () => {
+  errorHolder.classList.remove('d-flex');
+});
+
 // EMAIL VALIDATION
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const emaill = email.value;
+  if (!isLowerCase(emaill)) {
+    errorHolder.classList.add('d-flex');
+  } else {
+    errorHolder.style.display = 'none';
+    form.submit();
+  }
+});
