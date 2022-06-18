ff37271cf1bbcf7b3660263961597180d2614585
krisbobo <kiyongbohbo@gmail.com>
2022-06-18 20:52:10 +0100

    Fix linter errors

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const navToggle = document.querySelector('.nav-toggle');
 const ul = document.querySelector('.links');
 const close = document.querySelector('.close');
 const closeItems = document.querySelector('.desktop-nav');
-const popup = document.querySelector('.bg')
+const popup = document.querySelector('.bg');
 
 navToggle.addEventListener('click', () => {
   ul.classList.add('d-block');
@@ -16,61 +16,57 @@ closeItems.addEventListener('click', () => {
   ul.classList.remove('d-block');
 });
 
-
-
 const cards = [
   {
     id: 1,
     image: 'images/photo1.png',
-    class:'project-card',
-    pop:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley `,
+    class: 'project-card',
+    pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley ',
     name: 'Tonic',
-    languages: ['html','CSS', 'JavaScript'],
-    description: `A daily selection of privtely personalized reads; no accounts or sign-ups required`,
+    languages: ['html', 'CSS', 'JavaScript'],
+    description: 'A daily selection of privtely personalized reads; no accounts or sign-ups required',
     links: ['#', '#'],
-    pop:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
   },
   {
     id: 2,
     image: 'images/photo2.png',
-    class:'project-cardr',
+    class: 'project-cardr',
 
     name: 'Multi-post Stories',
-    languages: ['html','CSS', 'JavaScript'],
-    description: `A daily selection of privtely personalized reads; no accounts or sign-ups required`,
+    languages: ['html', 'CSS', 'JavaScript'],
+    description: 'A daily selection of privtely personalized reads; no accounts or sign-ups required',
     links: ['#', '#'],
-    pop:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
   {
     id: 3,
-    class:'project-card',
+    class: 'project-card',
 
     image: 'images/photo3.png',
     name: 'Tonic',
-    languages: ['html','CSS', 'JavaScript'],
-    description: `A daily selection of privtely personalized reads; no accounts or sign-ups required`,
+    languages: ['html', 'CSS', 'JavaScript'],
+    description: 'A daily selection of privtely personalized reads; no accounts or sign-ups required',
     links: ['#', '#'],
-    pop:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
   {
     id: 4,
-    class:'project-cardr',
+    class: 'project-cardr',
     image: 'images/photo4.png',
     name: 'Multi-post Stories',
-    languages: ['html','CSS', 'JavaScript'],
-    description: `A daily selection of privtely personalized reads; no accounts or sign-ups required`,
+    languages: ['html', 'CSS', 'JavaScript'],
+    description: 'A daily selection of privtely personalized reads; no accounts or sign-ups required',
     links: ['#', '#'],
-    pop:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-  }
-]
+    pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  },
+];
 
-const cardHolder = document.querySelector('.work-section')
+const cardHolder = document.querySelector('.work-section');
 
+function displayPopup(card) {
+  const temp = document.createElement('template');
 
-function displayPopup(card){
-  let temp = document.createElement('template')
-
-  temp.innerHTML= `
+  temp.innerHTML = `
   <div class="project-card pop-up-card">
             
             <div class="project-card2">
@@ -100,7 +96,7 @@ function displayPopup(card){
                   <div>
                     <div class="language1 inline">
                         <ul>
-                        ${card.languages.map((lang)=> `<li class="langu">${lang}</li>`).join('')}
+                        ${card.languages.map((lang) => `<li class="langu">${lang}</li>`).join('')}
 
                         </ul>
                     </div>
@@ -117,30 +113,28 @@ function displayPopup(card){
 </div>
         </div>
   
-  `
-popup.innerHTML = ``
-popup.append(temp.content)
+  `;
+  popup.innerHTML = '';
+  popup.append(temp.content);
 
-const closePopup = document.querySelector('.close-popup')
+  const closePopup = document.querySelector('.close-popup');
 
-  closePopup.addEventListener('click',()=> {
-    popup.classList.remove('d-popup')
-  })
+  closePopup.addEventListener('click', () => {
+    popup.classList.remove('d-popup');
+  });
 }
 
 function getItemId(id) {
-  
   cards.find((card) => {
-    if(card.id == id){
-      displayPopup(card)
+    if (card.id === id) {
+      displayPopup(card);
     }
-  })
+  });
 }
 
-
 cards.forEach((card) => {
-let temp = document.createElement('template');
-temp.innerHTML = `
+  const temp = document.createElement('template');
+  temp.innerHTML = `
 <div class="${card.class}" >
             <div class="project-card1">
                 <img src="${card.image}" alt="First Card" />
@@ -163,7 +157,7 @@ temp.innerHTML = `
                     </p>
                     <div class="language1 inline">
                         <ul>
-                        ${card.languages.map((lang)=> `<li class="langu">${lang}</li>`).join('')}
+                        ${card.languages.map((lang) => `<li class="langu">${lang}</li>`).join('')}
                             
 
                         </ul>
@@ -172,19 +166,13 @@ temp.innerHTML = `
                 <button id="${card.id}" type="button" class="btn-one seeProject">See project</button>
             </div>
         </div>
-`
-cardHolder.append(temp.content)
-
-
-})
-const btns = document.querySelectorAll('.seeProject')
+`;
+  cardHolder.append(temp.content);
+});
+const btns = document.querySelectorAll('.seeProject');
 btns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    getItemId(e.target.id)
-    popup.classList.add('d-popup')
-  })
-})
-
-
-
-
+    getItemId(e.target.id);
+    popup.classList.add('d-popup');
+  });
+});
